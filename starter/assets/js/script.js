e27b2bc06ec7fbecd7bf102b03b420333a0d91f4
andyvgumz <andyvgumz@yahoo.co.uk>
2023-09-18 23:04:18 +0100

    Index html and Script js Updated

--- starter/assets/js/script.js
+++ starter/assets/js/script.js
@@ -4,7 +4,10 @@ const weatherAPIKey = "ba9a603fe5bc71a41792577416da71ca"
 let searchInput = $("search-input");
 let searchForm = $("search-form");
 let searchHistory = []
-let searchHistoryContainer = $("#history")
+let searchHistoryContainer = $("#history");
+let forecastContainer = $("#forecast");
+let todayContainer = $("#today");
+
 
 //A function to render the history if needed
 function renderSearchHistory(search) {
@@ -32,6 +35,109 @@ function appendSearchHistory(search) {
     localStorage.setItem('history', JSON.stringify(searchHistory));
 }
 
+function renderCurrentWeather(city, weatherData) {
+   let date = moment().format("D/M/YYYY");
+   let tempC = weatherData["main"] ["temp"];
+   let windKph = weatherData["wind"] ["speed"];
+   let humidity = weatherData["main"]["humidity"];
+
+   let iconUrl = `https://api.openweathermap.org/img/w/${WeatherData.weather[0].icon}.png`;
+   let iconDescription = weatherData.weather[0].description || weatherData[0].main
+
+   let card = $("<div>")
+   let cardBody = $("<div>")
+   let weatherIcon = $("<img>")
+
+   let heading = $("<h2>")
+   let tempEl = $("<p>")
+   let windEl = $("<p>")
+
+   let humidityEl = $("<p>")
+
+   card.attr("class", "card");
+
+   cardBody.attr("class", "card-body");
+
+   card.append(cardBody);
+
+   heading.attr("class", "h3 card-title")
+   tempEl.attr("class", "card-text")
+   windEl.attr("class", "card-text")
+   humidityEl.attr("class","card-text");
+
+   heading.text(`${city}(${date})`)
+   weatherIcon.attr("src", iconUrl);
+   weatherIcon.attr("alt", iconDescription);
+
+   heading.append(weatherIcon);
+   tempEl.text(`Tenp ${tempC} C`)
+   windEl.text(`Wind ${windKph} KPH`)
+   humidityEl.text(`Humidity ${humidity} %`)
+   cardBody.append(heading, tempEl, windEl, humidityEl);
+
+   todayContainer.html("");
+   todayContainer.append(card);
+}
+
+function renderForecast(weatherData){
+    let headingCol = $("div");
+    let heading = $("<h4>");
+
+    headingCol.attr("class", "col-12");
+    heading.text("5 day forecast");
+    headingCol.append(heading);
+
+    forecastContainer.html("")
+
+    forecastContainer.append(headingCol);
+
+    let futureForecast = weatherData.filter(function(forecast){
+        return forecast.dt_txt.includes("12")
+    })
+
+    // console.log(futureForecast);
+
+    for(let i = 0; i <futureForecast.length; i++){
+        let iconUrl = `https://api.openweathermap.org/img/w/${futureForecast[i].weather[0].icon}.png`;
+        // console.log(iconUrl);
+        let iconDescription = futureForecast[i].weather[0].description;
+        let tempC = futureForecast[i].main.temp;
+        let humidity = futureForecast[i].main.humidity;
+        let windKhp = futureForecast[i].wind.speed;
+
+        let col = $("<div>")
+        let card = $("<div>")
+        let cardBody = $("<div>");
+        let cardTitle = $("<h5>");
+        let weatherIcon = $("<img>")
+        let tempEl = $("<p>")
+        let windEl = $("<p>")
+        let humidityEl = $("<p>")
+
+        col.append(card);
+        card.append(cardBody);
+        cardBody.append(cardTitle, weatherIcon, tempEl, windEl, humidityEl);
+
+        col.attr("class", "col-md");
+        card.attr("class", "card bg-primary h-100 text-white")
+        cardTitle.attr("class", "card-title")
+        tempEl.attr("class", "card-text")
+        windEl.attr("class", "card-text")
+        humidityEl.attr("class", "card-text");
+
+        cardTitle.text(moment(futureForecast[i].format("D/M/YYYY")));
+        weatherIcon.attr("src", iconURL);
+        weatherIcon.attr("alt", iconDescription);
+        tempEl.text(`Temp ${tempC} C`)
+        windEl.text(`Wind ${windKph} KPH`);
+        humidityEl.text(`Humidity ${humidity} %`);
+
+        forecastContainer.append(col)
+
+    }
+   
+}
+
 function fetchWeather(location){
     let lat = location.lat; //test coordinates
     let lon = location.lon;//test coordinates
@@ -45,7 +151,8 @@ function fetchWeather(location){
         url: queryWeatherURL,
         method:"GET",
     }).then(function(response){
-        console.log(response);
+        renderCurrentWeather(city, response.list[0])
+        renderForecast(response.list);
     })
 }
 
@@ -76,7 +183,7 @@ function initializeHistory(){
 }
 
 
-function submitSearchFrom(event) {
+function submitSearchForm(event) {
 
     event.preventDefault();
     let search = searchInput.val().trim()
@@ -85,9 +192,22 @@ function submitSearchFrom(event) {
     searchInput.val("");
 }
 
+function clickSearchHistory(event){
+    if(!$(event.target).hasClass("btn-history")){
+        return
+    }
+    let search = $(event.target).attr("data-search")
+    // alert(search)
+
+    featchCoord(search);
+    searchInput.val("")
+
+}
+
 
 initializeHistory();
-searchForm.on("submit", submitSearchFrom);
+searchForm.on("submit", submitSearchForm);
+searchHistoryContainer.on("click", clickSearchHistory)
 
 
 
